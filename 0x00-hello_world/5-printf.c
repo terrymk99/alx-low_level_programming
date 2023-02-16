4a87a0cf238da290f2b74b70cac793218b6341f5
terrymk99 <terrencesci@gmail.com>
2023-02-16 06:47:32 -0800

    Task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Use the function printf
+ * Description: You are not allowed to use the function puts
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
