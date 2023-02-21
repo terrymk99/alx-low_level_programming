e1951224025d957a57c15249d0c1c82513865461
terrymk99 <terrencesci@gmail.com>
2023-02-21 08:56:00 -0800

    Create a function that prints the 9x table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9x table
+ * @void: no params
+ * Return: void
+ */
+
+
+void times_table(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (j <= 9)
+	{
+		i = 0;
+		while (i <= 9)
+		{
+			if ((j * i) > 9)
+			{
+				_putchar(' ');
+				_putchar(((j * i) / 10) + '0');
+				_putchar(((j * i) % 10) + '0');
+			}
+			else
+			{
+				if (i != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar((j * i) + '0');
+			}
+			if (i != 9)
+				_putchar(',');
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
