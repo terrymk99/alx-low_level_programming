32e4937da504ee2c850e767f5dc9b915558ce5c3
terrymk99 <terrencesci@gmail.com>
2023-02-21 10:16:05 -0800

    Create a function where ∀n∈N n={n:x<98}

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 -  prints all natural numbers from n to 98
+ * @n: input integer
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d", i);
+		if (i < 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
