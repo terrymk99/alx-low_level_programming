5c1aede67eb8312023bbda4ad38042afeb078859
terrymk99 <terrencesci@gmail.com>
2023-02-21 14:15:07 -0800

    Create a function where ∀n∈N n={n:x<98}

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,15 +9,21 @@
 
 void print_to_98(int n)
 {
-	int i;
-
-	for (i = n; i <= 98; i++)
+	if (n < 98)
 	{
-		printf("%d", i);
-		if (i < 98)
+		while (n < 98)
 		{
-			printf(", ");
+			printf("%d, ", n);
+			n++;
 		}
 	}
-	printf("\n");
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
 }
