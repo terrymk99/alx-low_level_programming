a77d2d84b56cccf687eb67f856a6359116b17be5
terrymk99 <terrencesci@gmail.com>
2023-02-21 15:10:03 -0800

    Correct betty errors

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,20 @@
  * Return: Always 0.
  */
 
-int main() {
-    int sum = 0;
-    int i;
-    for (i = 1; i < 1024; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
-            sum += i;
-        }
-    }
-    printf("%d\n", sum);
-    return 0;
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	printf("%d\n", sum;
+	return (0);
 }
 
