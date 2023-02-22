cbd5b3e73c7595561459854457beac93ae171189
terrymk99 <terrencesci@gmail.com>
2023-02-22 09:26:12 -0800

    Create 0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints interger's polarity
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
