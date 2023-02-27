7b578da9d8053ed51b303c65be2c219a8b7cbab9
terrymk99 <terrencesci@gmail.com>
2023-02-27 09:17:13 -0800

    Create 0-reset_to_98.c, 0-main.c, main.h,  _putchar.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * reset_to_98 - Updated the value of 'n' to 98
+ * @n: integer pointer/update parameter
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
